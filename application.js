56a122e40dabaa1bcd7ae14b811481bad3a2aa4e
Egan Bailey <eganbailey1993@gmail.com>
2016-05-06 12:57:40 -0700

    Adding Styling and some more funcitons to the page

--- application.js
+++ application.js
@@ -1,25 +1,50 @@
-var todos = [];
+$(document).ready(function() {
+    $("#AddButton").click(addItem);
 
-$('document').ready(function() {
-    buildList();
-
-    $('#btnAdd').click(function() {
-        var newTodo = $('#txtTodo').val();
-        todos.push(newTodo);
-        $('#txtTodo').val("");
-        buildList();
+    
+    $("#newItemTextBox").keydown(function(event) {
+        var keycode = event.keyCode ? event.keyCode : event.which;
+        if(keycode == 13){
+            addItem();
+        }
     });
+
+    
+    $("#list").on("click", "a", function() {
+        var listItem = $(this).closest("li");
+        var itemName = listItem.find("span").text();
+        var message = "Are you sure you want to remove '" + itemName + "' from the list?";
+        if(window.confirm(message)) {
+            listItem.remove();
+        }
+    });
+
+    
+    $("#list").on("change", "input:checkbox",function () {
+        var item = $(this).closest("li").find("span");
+        if($(this).is(":checked")) {
+            item.addClass("completed");
+        }
+        else
+            item.removeClass("completed"); 
+    });  
 });
 
-function buildList() {
-    $('#todoList').html("");
-    todos.forEach(function(todo, index) {
-        $('#todoList').append('<li>' + todo + ' <button class="btn-delete" data-todo-index="' + index + '">Delete</button></li>');
-    });
+function addItem() {
+    var toDo = $("#newItemTextBox").val();
+    if(toDo.trim().length === 0) {
+        alert("You must enter an item to be added.");
+        return;
+    }
 
-    $('.btn-delete').click(function() {
-        var index = $(this).data('todoIndex');
-        todos.splice(index, 1);
-        buildList();
-    })
+    var listItem = createListItem(toDo);
+    $("#list").append(listItem);
+    $("#newItemTextBox").val("");
+}
+
+function createListItem(toDo) {
+    var listItem = "<li class='ui-state-default'><input type='checkbox'>"; 
+    listItem += "<span>" + toDo + "</span>";
+    listItem += "<a href='#'>remove</a></li>";
+    return listItem; 
 }
