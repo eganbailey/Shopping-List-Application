6a214f66971869f0440bf5b3fe53910c3a829382
Egan Bailey <eganbailey1993@gmail.com>
2016-05-06 12:30:08 -0700

    First Commit

---
+++ application.js
@@ -0,0 +1,25 @@
+var todos = [];
+
+$('document').ready(function() {
+    buildList();
+
+    $('#btnAdd').click(function() {
+        var newTodo = $('#txtTodo').val();
+        todos.push(newTodo);
+        $('#txtTodo').val("");
+        buildList();
+    });
+});
+
+function buildList() {
+    $('#todoList').html("");
+    todos.forEach(function(todo, index) {
+        $('#todoList').append('<li>' + todo + ' <button class="btn-delete" data-todo-index="' + index + '">Delete</button></li>');
+    });
+
+    $('.btn-delete').click(function() {
+        var index = $(this).data('todoIndex');
+        todos.splice(index, 1);
+        buildList();
+    })
+}
